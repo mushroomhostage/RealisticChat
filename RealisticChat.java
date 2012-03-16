079a465ff1f7265172dd108edd8e3ba1b2889587
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-03-15 20:34:14 -0700

    Custom delivery, and first stab at probability-based message letter dropping

--- RealisticChat.java
+++ RealisticChat.java
@@ -1,9 +1,10 @@
 package me.exphc.RealisticChat;
 
-import java.util.logging.Logger;
 
+import java.util.logging.Logger;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Random;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.*;
@@ -14,8 +15,11 @@ import org.bukkit.*;
 
 class RealisticChatListener implements Listener {
     RealisticChat plugin;
+    Random random;
 
     public RealisticChatListener(RealisticChat plugin) {
+        this.random = new Random();
+
         this.plugin = plugin;
 
         Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
@@ -25,10 +29,8 @@ class RealisticChatListener implements Listener {
     public void onPlayerChat(PlayerChatEvent event) {
         Player sender = event.getPlayer();
 
-        // Get a copy of the recipients, so we can modify it (for some reason, no clone()?)
-        Set<Player> recipients = new HashSet<Player>(event.getRecipients());
-
-        for (Player recipient: recipients) {
+        // Send to recipients
+        for (Player recipient: event.getRecipients()) {
             if (sender.equals(recipient)) {
                 // Talking to ourselves
                 // TODO: still scramble?
@@ -38,7 +40,6 @@ class RealisticChatListener implements Listener {
             if (!sender.getWorld().equals(recipient.getWorld())) {
                 // Not in this world!
                 // TODO: cross-world communication device?
-                event.getRecipients().remove(recipient);
                 continue;
             }
 
@@ -46,16 +47,45 @@ class RealisticChatListener implements Listener {
 
             plugin.log.info("distance="+distance);
 
+            String message = event.getMessage();
+
             // Limit distance
             if (distance > plugin.getConfig().getInt("hearingRangeMeters", 50)) {
-                // TODO: earphones?
-                event.getRecipients().remove(recipient);
+                // TODO: earphones? to receive further
                 continue;
             }
             if (distance > plugin.getConfig().getInt("scrambleRangeMeters", 25)) {
-                // TODO: delete random letters
+                // Delete random letters
+                StringBuilder newMessage = new StringBuilder();
+
+                // This string character iteration method is cumbersome, but it is
+                // the most correct, especially if players are using plane 1 characters
+                // see http://mindprod.com/jgloss/codepoint.html
+                int i = 0;
+                double clarity = 0.5;    // probability of getting through
+                while (i < message.length()) {
+                    int c = message.codePointAt(i);
+                    i += Character.charCount(c);
+
+                    if (random.nextDouble() < clarity) {
+                        newMessage.appendCodePoint(c);
+                    } else {
+                        newMessage.append(' ');
+                    }
+                }
+
+                deliverMessage(recipient, sender, new String(newMessage));
+            } else {
+                deliverMessage(recipient, sender, message);
             }
         }
+
+        // Deliver the message manually, so we can customize the chat display 
+        event.setCancelled(true);
+    }
+
+    private void deliverMessage(Player recipient, Player sender, String message) {
+        sender.sendMessage(ChatColor.GREEN + sender.getDisplayName() + ": " + message);
     }
 }
 
